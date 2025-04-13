821cdb2fbc5043941243d2d6f6cbfe3c001b44fb
Gowri D <140594338+Gowridhen@users.noreply.github.com>
2025-04-13 20:03:31 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+document.addEventListener('DOMContentLoaded', () => {
+  AOS.init({
+    once: true,
+    duration: 1000
+  });
 
+  const bars = document.querySelectorAll('.bar-fill');
+  bars.forEach(bar => {
+    const width = bar.style.width;
+    bar.style.width = 0;
+    setTimeout(() => {
+      bar.style.width = width;
+    }, 500);
+  });
+});

689d8b9a6dc356704b5c2c4c3f5ed4257a7e96fe
Gowri D <140594338+Gowridhen@users.noreply.github.com>
2025-04-13 20:16:01 +0530

    Update script.js

--- script.js
+++ script.js
@@ -13,3 +13,19 @@ document.addEventListener('DOMContentLoaded', () => {
     }, 500);
   });
 });
+
+document.getElementById('theme1').addEventListener('click', () => {
+  document.body.classList.remove('dark-mode', 'light-mode');
+  document.body.classList.add('blue-theme');
+});
+
+document.getElementById('theme2').addEventListener('click', () => {
+  document.body.classList.remove('blue-theme', 'light-mode');
+  document.body.classList.add('dark-mode');
+});
+
+document.getElementById('theme3').addEventListener('click', () => {
+  document.body.classList.remove('blue-theme', 'dark-mode');
+  document.body.classList.add('light-mode');
+});
+
